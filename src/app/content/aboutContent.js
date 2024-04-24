ed7a539660c934a9a1e763718ef8c32729377e53
skarokin <sean.kim0327@gmail.com>
2024-04-24 19:21:25 -0400

    transparency, better styling

--- src/app/content/aboutContent.js
+++ src/app/content/aboutContent.js
@@ -2,11 +2,9 @@ const aboutContent =
 "# My Programming Journey\n" +
 "##### 2017:\n" +
 "- Tried to read Java for Dummies in the 7th grade, couldn't even set up Eclipse and declared programming was too hard 😂\n" +
-"&nbsp;\n" +
 "##### 2022:\n" +
 "- Started my first year at Rutgers University, taking Intro CS and Linear Algebra\n" +
-"- Wrote a linear regression calculator in Java for a hackathon. Implemented all the math from scratch. This was my first time coming up with algorithms and putting them into code\n" +
-"&nbsp;\n" +
+"- Wrote a linear regression calculator in Java for a hackathon. Implemented all the math from scratch.\n" +
 "##### 2023:\n" +
 "- Took DSA and Discrete Structures I\n" +
 "- Made an Obsidian-like note taking app with Electron, using pure JavaScript/HTML/CSS\n" +
@@ -14,7 +12,6 @@ const aboutContent =
 "  - On March 24, 2024 at 11:59 PM, my 90 day GCP free trial ran out... unfortunately marked the end of deployment :(\n" +
 "- Took Computer Architecture, Discrete Structures II, and Linear Programming\n" +
 "- Built an object detection based CAPTCHA system by training my own YOLOv4-tiny model. Made a customizable image augmentation pipeline\n" +
-"&nbsp;\n" +
 "##### 2024:\n" +
 "- Took Design and Analysis of Computer Algorithms, Data Management with Python, and Databases\n" +
 "- Did 3 months of research on POS tagging and dependency parsing. Attempted to create a HMM POS tagger from scratch and a shift-reduce dependency parser from scratch... miserably failed both\n" +
@@ -23,13 +20,9 @@ const aboutContent =
 "- Made this portfolio website you're looking at right now with Next.js\n" +
 "- Took Intro to Deep Learning and Principles of Programming Languages\n" +
 "&nbsp;\n" +
-"# Fun (i guess) Facts\n" +
-"- I love animanga, am the IGL for a Valorant team, and help coach my high school's Esports team\n" +
-"- I love typing. 210 WPM on 15s test (78th global) and 177 WPM on 60s (129th global)\n" +
-"- I've done 7 coast-to-coast road trips and have visited almost every major national park\n" +
-"- I lived in Oregon from 2018-2019\n" +
-"- In 2018, I heard an Aimer song for the first time in an anime ending. She's been my favorite artist ever since\n" +
-"- In 2021, I started learning Japanese... mainly just to understand Aimer lyrics\n" +
-"- In 2022, I started going to the gym and always look forward to leg day\n";
+"# Other\n" +
+"- I'm the IGL for a Valorant team, and help coach my high school's Esports team\n" +
+"- I type 210 WPM on the 15s test (78th global) and 177 WPM on the 60s test (129th global)\n" +
+"- I've done 7 coast-to-coast road trips and have visited almost every major national park\n"
 
 export default aboutContent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,20 @@ const Background: React.FC<{ children: ReactNode }> = ({ children }) => {
 
 export default function Home() {
 
-  const [content, setContent] = useState<string>(contentMap['/readme.md'].content);
-  const [url, setUrl] = useState<string>(contentMap['/readme.md'].url);
-  const [children, setChildren] = useState<ReactNode>(contentMap['/readme.md'].children);
   const [currentPage, setCurrentPage] = useState<string>('/readme.md');
 
   const changePage = (newPage: string) => {
     setCurrentPage(newPage);
-    setContent(contentMap[newPage as keyof typeof contentMap].content);
-    setUrl(contentMap[newPage as keyof typeof contentMap].url);
-    setChildren(contentMap[newPage as keyof typeof contentMap].children);
   }
+
+  const currentPageData = contentMap[currentPage as keyof typeof contentMap];
+  const { content, url, children } = currentPageData;
 
   return (
     <>
       <Background>
-        <section className={'min-h-screen p-8 md:p-24 w-10/12'}>
-          <div className="flex-row md:flex items-start w-full">
+        <section className={'min-h-screen p-8 md:p-24 w-full'}>
+          <div className="flex flex-col lg:flex-row items-start">
             <FS changePage={changePage}/>
             <MDEditor url={url} content={content}>
               {children}
