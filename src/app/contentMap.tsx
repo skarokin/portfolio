012699619e263c911dce58d7e80840dade46ebf8
Sean Kim <sean.kim0327@gmail.com>
2024-05-20 20:32:45 -0400

    new content and resume changes

--- src/app/contentMap.tsx
+++ src/app/contentMap.tsx
@@ -1,77 +1,99 @@
-import readMeContent from './content/readMeContent.mdx';
-import aboutContent from './content/aboutContent.mdx';
-import resumeContent from './content/resumeContent.mdx';
-import linearRegressionContent from './content/linearRegressionContent.mdx';
-import MYnotemdContent from './content/MYnotemdContent.mdx';
-import reftypeContent from './content/reftypeContent.mdx';
-import pycaptchaContent from './content/pycaptchaContent.mdx';
-import depparseGrammarContent from './content/depparseGrammarContent.mdx';
+import readMeContent from "./content/readMeContent.mdx";
+import aboutContent from "./content/aboutContent.mdx";
+import resumeContent from "./content/resumeContent.mdx";
+import linearRegressionContent from "./content/linearRegressionContent.mdx";
+import MYnotemdContent from "./content/MYnotemdContent.mdx";
+import reftypeContent from "./content/reftypeContent.mdx";
+import pycaptchaContent from "./content/pycaptchaContent.mdx";
+import grammaCyContent from "./content/grammaCyContent.mdx";
 
-const rawReadMeContentModule = require('!!raw-loader!./content/readMeContent.mdx');
-const rawReadMeContent = rawReadMeContentModule.default.replace(/import .*;\s*/g, '');
+const rawReadMeContentModule = require("!!raw-loader!./content/readMeContent.mdx");
+const rawReadMeContent = rawReadMeContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
-const rawAboutContentModule = require('!!raw-loader!./content/aboutContent.mdx');
-const rawAboutContent = rawAboutContentModule.default.replace(/import .*;\s*/g, '');
+const rawAboutContentModule = require("!!raw-loader!./content/aboutContent.mdx");
+const rawAboutContent = rawAboutContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
-const rawResumeContentModule = require('!!raw-loader!./content/resumeContent.mdx');
-const rawResumeContent = rawResumeContentModule.default.replace(/import .*;\s*/g, '');
+const rawResumeContentModule = require("!!raw-loader!./content/resumeContent.mdx");
+const rawResumeContent = rawResumeContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
-const rawLinearRegressionContentModule = require('!!raw-loader!./content/linearRegressionContent.mdx');
-const rawLinearRegressionContent = rawLinearRegressionContentModule.default.replace(/import .*;\s*/g, '');
+const rawLinearRegressionContentModule = require("!!raw-loader!./content/linearRegressionContent.mdx");
+const rawLinearRegressionContent =
+  rawLinearRegressionContentModule.default.replace(/import .*;\s*/g, "");
 
-const rawMYnotemdContentModule = require('!!raw-loader!./content/MYnotemdContent.mdx');
-const rawMYnotemdContent = rawMYnotemdContentModule.default.replace(/import .*;\s*/g, '');
+const rawMYnotemdContentModule = require("!!raw-loader!./content/MYnotemdContent.mdx");
+const rawMYnotemdContent = rawMYnotemdContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
-const rawReftypeContentModule = require('!!raw-loader!./content/reftypeContent.mdx');
-const rawReftypeContent = rawReftypeContentModule.default.replace(/import .*;\s*/g, '');
+const rawReftypeContentModule = require("!!raw-loader!./content/reftypeContent.mdx");
+const rawReftypeContent = rawReftypeContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
-const rawPycaptchaContentModule = require('!!raw-loader!./content/pycaptchaContent.mdx');
-const rawPycaptchaContent = rawPycaptchaContentModule.default.replace(/import .*;\s*/g, '');
+const rawPycaptchaContentModule = require("!!raw-loader!./content/pycaptchaContent.mdx");
+const rawPycaptchaContent = rawPycaptchaContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
-const rawDepparseGrammarContentModule = require('!!raw-loader!./content/depparseGrammarContent.mdx');
-const rawDepparseGrammarContent = rawDepparseGrammarContentModule.default.replace(/import .*;\s*/g, '');
+const rawGrammaCyContentModule = require("!!raw-loader!./content/grammaCyContent.mdx");
+const rawGrammaCyContent = rawGrammaCyContentModule.default.replace(
+  /import .*;\s*/g,
+  "",
+);
 
 const contentMap = {
-    '#readme.md': {
-        content: readMeContent,
-        rawContent: rawReadMeContent,
-        url: '/readme.md',
-    },
-    '#about.md': {
-        content: aboutContent,
-        rawContent: rawAboutContent,
-        url: '/about.md',
-    },
-    '#resume.md': {
-        content: resumeContent,
-        rawContent: rawResumeContent,
-        url: '/resume.md',
-    },
-    '#projects/LinearRegression.md': {
-        content: linearRegressionContent,
-        rawContent: rawLinearRegressionContent,
-        url: '/projects/LinearRegression.md',
-    },
-    '#projects/MYnotemd.md': {
-        content: MYnotemdContent,
-        rawContent: rawMYnotemdContent,
-        url: '/projects/MYnotemd.md',
-    },
-    '#projects/reftype.md': {
-        content: reftypeContent,
-        rawContent: rawReftypeContent,
-        url: '/projects/reftype.md',
-    },
-    '#projects/pycaptcha.md': {
-        content: pycaptchaContent,
-        rawContent: rawPycaptchaContent,
-        url: '/projects/pycaptcha.md',
-    },
-    '#projects/depparse-grammar.md': {
-        content: depparseGrammarContent,
-        rawContent: rawDepparseGrammarContent,
-        url: '/projects/depparse-grammar.md',
-    },
+  "#readme.md": {
+    content: readMeContent,
+    rawContent: rawReadMeContent,
+    url: "/readme.md",
+  },
+  "#about.md": {
+    content: aboutContent,
+    rawContent: rawAboutContent,
+    url: "/about.md",
+  },
+  "#resume.md": {
+    content: resumeContent,
+    rawContent: rawResumeContent,
+    url: "/resume.md",
+  },
+  "#projects/LinearRegression.md": {
+    content: linearRegressionContent,
+    rawContent: rawLinearRegressionContent,
+    url: "/projects/LinearRegression.md",
+  },
+  "#projects/MYnotemd.md": {
+    content: MYnotemdContent,
+    rawContent: rawMYnotemdContent,
+    url: "/projects/MYnotemd.md",
+  },
+  "#projects/reftype.md": {
+    content: reftypeContent,
+    rawContent: rawReftypeContent,
+    url: "/projects/reftype.md",
+  },
+  "#projects/pycaptcha.md": {
+    content: pycaptchaContent,
+    rawContent: rawPycaptchaContent,
+    url: "/projects/pycaptcha.md",
+  },
+  "#projects/grammaCy.md": {
+    content: grammaCyContent,
+    rawContent: rawGrammaCyContent,
+    url: "/projects/depparse-grammar.md",
+  },
 };
 
 export default contentMap;
--- src/app/fs.tsx
+++ src/app/fs.tsx
@@ -1,82 +1,121 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
-import { faFile as farFile } from '@fortawesome/free-regular-svg-icons';
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder, faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+import { faFile as farFile } from "@fortawesome/free-regular-svg-icons";
 
 export default function FS() {
+  const changePage = (newPage: string) => {
+    window.location.hash = newPage.startsWith("/")
+      ? newPage.substring(1)
+      : newPage;
+  };
 
-    const changePage = (newPage: string) => {
-        window.location.hash = newPage.startsWith('/') ? newPage.substring(1) : newPage;
-    };
-    
-    const [isRootOpen, setIsRootOpen] = useState<boolean>(true);
-    const [isProjectsOpen, setIsProjectsOpen] = useState<boolean>(true);
+  const [isRootOpen, setIsRootOpen] = useState<boolean>(true);
+  const [isProjectsOpen, setIsProjectsOpen] = useState<boolean>(true);
 
-    return (
-        <div className="whitespace-nowrap bg-bgColor/50 backdrop-blur-[2px] shadow-2xl min-h-fit mb-8 min-w-72 border-2 border-slate-700 rounded py-2 px-4 mr-4 font-mono text-slate-300">
-            <div onClick={() => setIsRootOpen(!isRootOpen)} className="items-center cursor-pointer">
-                <FontAwesomeIcon icon={isRootOpen ? faFolderOpen : faFolder} className="mr-2 w-4"/>
-                sean_kim
-            </div>
-            <AnimatePresence>
-                {isRootOpen && (
+  return (
+    <div className="whitespace-nowrap bg-bgColor/50 backdrop-blur-[2px] shadow-2xl min-h-fit mb-8 min-w-72 border-2 border-slate-700 rounded py-2 px-4 mr-4 font-mono text-slate-300">
+      <div
+        onClick={() => setIsRootOpen(!isRootOpen)}
+        className="items-center cursor-pointer"
+      >
+        <FontAwesomeIcon
+          icon={isRootOpen ? faFolderOpen : faFolder}
+          className="mr-2 w-4"
+        />
+        sean_kim
+      </div>
+      <AnimatePresence>
+        {isRootOpen && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            className="mt-2"
+          >
+            <ul className="pl-4 text-sm lg:text-base mb-2">
+              <li>
+                <div
+                  onClick={() => setIsProjectsOpen(!isProjectsOpen)}
+                  className="flex flex-row items-center cursor-pointer mb-2"
+                >
+                  <FontAwesomeIcon
+                    icon={isProjectsOpen ? faFolderOpen : faFolder}
+                    className="mr-2 w-4"
+                  />
+                  projects
+                </div>
+                <AnimatePresence>
+                  {isProjectsOpen && (
                     <motion.div
-                        initial={{ opacity: 0, height: 0 }}
-                        animate={{ opacity: 1, height: "auto" }}
-                        exit={{ opacity: 0, height: 0 }}
-                        className="mt-2"
+                      initial={{ opacity: 0, height: 0 }}
+                      animate={{ opacity: 1, height: "auto" }}
+                      exit={{ opacity: 0, height: 0 }}
+                      className="mt-2"
                     >
-                        <ul className="pl-4 text-sm lg:text-base mb-2">
-                            <li>
-                                <div onClick={() => setIsProjectsOpen(!isProjectsOpen)} className="flex flex-row items-center cursor-pointer mb-2">
-                                    <FontAwesomeIcon icon={isProjectsOpen ? faFolderOpen : faFolder} className="mr-2 w-4"/>
-                                    projects
-                                </div>
-                                <AnimatePresence>
-                                    {isProjectsOpen && (
-                                        <motion.div
-                                            initial={{ opacity: 0, height: 0 }}
-                                            animate={{ opacity: 1, height: "auto" }}
-                                            exit={{ opacity: 0, height: 0 }}
-                                            className="mt-2"
-                                        >
-                                            <ul className="pl-4 text-sm lg:text-base mb-2">
-                                                <li className="cursor-pointer mb-2" onClick={() => changePage('/projects/LinearRegression.md')}>
-                                                    <FontAwesomeIcon icon={farFile}/> LinearRegression.md
-                                                </li>
-                                                <li className="cursor-pointer mb-2" onClick={() => changePage('/projects/MYnotemd.md')}>
-                                                    <FontAwesomeIcon icon={farFile}/> MYnotemd.md
-                                                </li>
-                                                <li className="cursor-pointer mb-2" onClick={() => changePage('/projects/reftype.md')}>
-                                                    <FontAwesomeIcon icon={farFile}/> reftype.md
-                                                </li>
-                                                <li className="cursor-pointer mb-2" onClick={() => changePage('/projects/pycaptcha.md')}>
-                                                    <FontAwesomeIcon icon={farFile}/> pycaptcha.md
-                                                </li>
-                                                <li className="cursor-pointer mb-2" onClick={() => changePage('/projects/depparse-grammar.md')}>
-                                                    <FontAwesomeIcon icon={farFile}/> depparse-grammar.md
-                                                </li>
-                                            </ul>
-                                        </motion.div>
-                                    )}
-                                </AnimatePresence>
-                            </li>
-                            <li className="cursor-pointer mb-2" onClick={() => changePage('/readme.md')}>
-                                <FontAwesomeIcon icon={farFile}/> readme.md
-                            </li>
-                            <li className="cursor-pointer mb-2" onClick={() => changePage('/about.md')}>
-                                <FontAwesomeIcon icon={farFile}/> about.md
-                            </li>
-                            <li className="cursor-pointer mb-2" onClick={() => changePage('/resume.md')}>
-                                <FontAwesomeIcon icon={farFile}/> resume.md
-                            </li>
-                        </ul>
+                      <ul className="pl-4 text-sm lg:text-base mb-2">
+                        <li
+                          className="cursor-pointer mb-2"
+                          onClick={() =>
+                            changePage("/projects/LinearRegression.md")
+                          }
+                        >
+                          <FontAwesomeIcon icon={farFile} /> LinearRegression.md
+                        </li>
+                        <li
+                          className="cursor-pointer mb-2"
+                          onClick={() => changePage("/projects/MYnotemd.md")}
+                        >
+                          <FontAwesomeIcon icon={farFile} /> MYnotemd.md
+                        </li>
+                        <li
+                          className="cursor-pointer mb-2"
+                          onClick={() => changePage("/projects/reftype.md")}
+                        >
+                          <FontAwesomeIcon icon={farFile} /> reftype.md
+                        </li>
+                        <li
+                          className="cursor-pointer mb-2"
+                          onClick={() => changePage("/projects/pycaptcha.md")}
+                        >
+                          <FontAwesomeIcon icon={farFile} /> pycaptcha.md
+                        </li>
+                        <li
+                          className="cursor-pointer mb-2"
+                          onClick={() => changePage("/projects/grammaCy.md")}
+                        >
+                          <FontAwesomeIcon icon={farFile} /> grammaCy.md
+                        </li>
+                      </ul>
                     </motion.div>
-                )}
-            </AnimatePresence>
-        </div>
-    )
+                  )}
+                </AnimatePresence>
+              </li>
+              <li
+                className="cursor-pointer mb-2"
+                onClick={() => changePage("/readme.md")}
+              >
+                <FontAwesomeIcon icon={farFile} /> readme.md
+              </li>
+              <li
+                className="cursor-pointer mb-2"
+                onClick={() => changePage("/about.md")}
+              >
+                <FontAwesomeIcon icon={farFile} /> about.md
+              </li>
+              <li
+                className="cursor-pointer mb-2"
+                onClick={() => changePage("/resume.md")}
+              >
+                <FontAwesomeIcon icon={farFile} /> resume.md
+              </li>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }
